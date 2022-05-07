0094d0dd0a7acd3355eb72508b0707583897a57d
Eneko96 <imr.aem2526@gmail.com>
2022-05-07 22:18:18 +0200

    added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,18 @@
 import { useTheme as useNextTheme } from "next-themes";
-import { useTheme, Text, Switch, Container, StyledCardHeader } from "@nextui-org/react";
+import { useTheme, Text, Switch, Container, StyledCardHeader, Button } from "@nextui-org/react";
 
 export default function Header() {
   const { isDark, type, theme } = useTheme();
   const { setTheme } = useNextTheme();
   console.log(theme?.colors.accents1)
   return (
-    <Container display="flex" alignContent="center" style={{ height: '3rem' }}>
-      <Text>This is Header</Text>
+    <Container display="flex" justify="space-around" alignContent="center" style={{ height: '3rem' }}>
+      <Button light>Home</Button>
+      <Button light>Blog</Button>
+      <Button light>Bookshelf</Button>
       <Switch
         checked={isDark}
+        css={{ alignSelf: "center" }}
         onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
       />
     </Container >
